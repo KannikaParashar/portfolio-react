3378da5781ac73dffa188f73cde88e6fc269346d
KannikaParashar <104681427+KannikaParashar@users.noreply.github.com>
2023-06-06 16:49:26 +0530

    .

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between bg-[#0a192f] text-gray-300 '>
+      
+    </div>
+  )
+}
+
+export default NavBar
